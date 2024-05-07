cf5a61a05cc538857b48205a75df08d2b517a3a8
Igor Baranov <ib2044@mail.ru>
2024-05-07 07:18:49 +0300

    simplify And tee, switch, Or tee, switch

--- pkg/rop/group/group.go
+++ pkg/rop/group/group.go
@@ -3,7 +3,6 @@ package group
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/ib-77/rop/pkg/rop"
 )
 
@@ -11,73 +10,88 @@ const (
 	EmptyResult = "empty result"
 )
 
-// AndWithCtx TODO ctx done
-func AndWithCtx[In any](ctx context.Context,
-	validateF func(ctx context.Context, resId int, in rop.Result[In]) rop.Result[In],
-	accumF func(ctx context.Context, resId int, in rop.Result[In]) rop.Result[In],
-	fs ...func(ctx context.Context) rop.Result[In]) rop.Result[In] {
+func AndTeeWithCtx[In any](ctx context.Context, input rop.Result[In],
+	fs ...func(ctx context.Context, in rop.Result[In]) rop.Result[In]) rop.Result[In] {
 
-	if len(fs) == 0 {
-		return rop.Fail[In](fmt.Errorf(EmptyResult))
+	if !input.IsSuccess() {
+		return input
 	}
 
-	var accum rop.Result[In]
-	for id, f := range fs {
+	res := input
+	for _, f := range fs {
 
-		r := f(ctx)
+		res = f(ctx, res)
 
-		if !r.IsSuccess() {
-			if validateF != nil {
-
-				r = validateF(ctx, id, r)
-				if !r.IsSuccess() {
-					return rop.Fail[In](r.Err())
-				}
-
-			} else {
-				return rop.Fail[In](r.Err())
-			}
-		}
-
-		if accumF != nil {
-			accum = accumF(ctx, id, r)
-		} else {
-			accum = r // last result
+		if !res.IsSuccess() {
+			return rop.Fail[In](res.Err())
 		}
 	}
-	return accum
+
+	return res
 }
 
-// OrWithCtx TODO ctx done
-func OrWithCtx[In any](ctx context.Context,
-	validateF func(ctx context.Context, resId int, in rop.Result[In]) rop.Result[In],
-	fs ...func(ctx context.Context) rop.Result[In]) rop.Result[In] {
+func AndSwitchWithCtx[In, Out any](ctx context.Context, input rop.Result[In],
+	switchF func(ctx context.Context, r rop.Result[In]) rop.Result[Out],
+	fs ...func(ctx context.Context, in rop.Result[In]) rop.Result[In]) rop.Result[Out] {
 
-	if len(fs) == 0 {
-		return rop.Fail[In](fmt.Errorf(EmptyResult))
+	if !input.IsSuccess() {
+		return switchF(ctx, input)
 	}
 
-	var err error = nil
-	for id, f := range fs {
+	res := input
+	for _, f := range fs {
 
-		r := f(ctx)
+		res = f(ctx, res)
+
+		if !res.IsSuccess() {
+			return rop.Fail[Out](res.Err())
+		}
+	}
+
+	return switchF(ctx, res)
+}
+
+func OrTeeWithCtx[In any](ctx context.Context, input rop.Result[In],
+	fs ...func(ctx context.Context, in rop.Result[In]) rop.Result[In]) rop.Result[In] {
+
+	if !input.IsSuccess() {
+		return input
+	}
+
+	var err error
+	for _, f := range fs {
+
+		r := f(ctx, input)
 
 		if r.IsSuccess() {
-
-			if validateF != nil {
-
-				r = validateF(ctx, id, r)
-				if r.IsSuccess() {
-					return r
-				}
-
-			} else {
-				return r
-			}
+			return r
 		}
 
 		err = rop.Iif(err == nil, r.Err(), errors.Join(err, r.Err()))
 	}
 
 	return rop.Fail[In](err)
+}
+
+func OrSwitchWithCtx[In, Out any](ctx context.Context, input rop.Result[In],
+	switchF func(ctx context.Context, r rop.Result[In]) rop.Result[Out],
+	fs ...func(ctx context.Context, in rop.Result[In]) rop.Result[In]) rop.Result[Out] {
+
+	if !input.IsSuccess() {
+		return switchF(ctx, input)
+	}
+
+	var err error
+	for _, f := range fs {
+
+		r := f(ctx, input)
+
+		if r.IsSuccess() {
+			return switchF(ctx, r)
+		}
+
+		err = rop.Iif(err == nil, r.Err(), errors.Join(err, r.Err()))
+	}
+
+	return rop.Fail[Out](err)
 }
